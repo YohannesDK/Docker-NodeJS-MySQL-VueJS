dcb243154d8533733413ef11c1092b6782be9e15
YohannesDK <kassaye85@gmail.com>
2021-01-06 23:40:24 -0800

    weird

--- todoapp/src/services/api.ts
+++ todoapp/src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://127.0.0.1:3000/api',
+  // baseURL: process.env.BASE_URL,
+  baseURL: 'http://localhost:3000/api',
 });
